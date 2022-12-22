f3cb8126726e18c0a88b1c23a5b2c7c10a210e82
Daniel Vettorazi <daniel.vettorazi@gmail.com>
2022-12-22 14:19:34 -0800

    added new files

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import XR8Scene from './XR8Scene'
+import { useRef, useEffect } from 'react'
+import './App.css'
+import { Canvas } from '@react-three/fiber'
+import { DreiRefraction } from './examples'
+
+function App() {
+  const canvasRef = useRef()
+  const R3Scene = useRef()
+
+  let { onxrloaded, cubeCamera } = XR8Scene(canvasRef, R3Scene)
+
+  useEffect(() => {
+    XRExtras.Loading.showLoading({ onxrloaded })
+  }, [])
+
+  return (
+    <div className="App">
+      <Canvas style={{ position: 'absolute' }}>
+        <scene ref={R3Scene}>
+          <ambientLight />
+          <pointLight position={[10, 15, 10]} />
+
+          <DreiRefraction envMap={cubeCamera.renderTarget.texture} />
+        </scene>
+      </Canvas>
+      <canvas ref={canvasRef} width="480" height="640" style={{ position: 'absolute' }}></canvas>
+    </div>
+  )
+}
+
+export default App
